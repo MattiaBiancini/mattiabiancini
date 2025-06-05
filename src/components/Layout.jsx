b996c72d0bc94fd69719488f28a55285bb0b8bbf
Biancini Mattia <m.biancini1@campus.unimib.it>
2025-06-05 23:39:54 +0200

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div>
+      <nav style={{ marginBottom: 20 }}>
+        <Link to="/" style={{ marginRight: 10 }}>Home</Link>
+        <Link to="/about" style={{ marginRight: 10 }}>About</Link>
+        <Link to="/projects" style={{ marginRight: 10 }}>Projects</Link>
+        <Link to="/swimming" style={{ marginRight: 10 }}>Swimming</Link>
+        <Link to="/contacts">Contacts</Link>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
---
+++ src/pages/Swimming.jsx
@@ -0,0 +1,8 @@
+export default function Swimming() {
+  return (
+    <div>
+      <h1>Swimming</h1>
+      <p>Share your swimming experiences or achievements here.</p>
+    </div>
+  );
+}
