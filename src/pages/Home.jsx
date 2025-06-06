cc1f0a900fd33225b728cb29fab24b1dc463d369
Biancini Mattia <m.biancini1@campus.unimib.it>
2025-06-06 18:10:08 +0200

    MagicPortfolio

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Homepage</h1>
-      <p>Welcome to my portfolio!</p>
-    </div>
-  );
-}
--- src/pages/Projects.jsx
+++
@@ -1,8 +0,0 @@
-export default function Projects() {
-  return (
-    <div>
-      <h1>Projects</h1>
-      <p>Here you can showcase your projects.</p>
-    </div>
-  );
-}
