96299b57dd90fbe6e45a25e908315fb8a3f2453b
Biancini Mattia <m.biancini1@campus.unimib.it>
2025-06-07 17:36:33 +0200

    Update About Page

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -64,7 +64,7 @@ const about = {
   title: `About – ${person.name}`,
   description: `Meet ${person.name}, ${person.role} from ${person.location}`,
   tableOfContent: {
-    display: true,
+    display: false,
     subItems: false,
   },
   avatar: {
