2c94d382f74c6bb0d5ae485dfb83ef211c08ca50
Biancini Mattia <m.biancini1@campus.unimib.it>
2025-06-08 09:59:14 +0200

    Bugfixing

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -47,8 +47,8 @@ const home = {
   headline: <>Building bridges between design and code</>,
   featured: {
     display: true,
-    title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
-    href: "/work/building-once-ui-a-customizable-design-system",
+    title: <>Recent project: <strong className="ml-4">Performance Variability</strong></>,
+    href: "/projects/performance-variability",
   },
   subline: (
     <>
@@ -372,7 +372,7 @@ const projects = {
       title: "PC Builder",
       technologies: [
         {
-          name: "Android",
+          name: "android",
           link: "https://www.android.com/",
         },
       ],
