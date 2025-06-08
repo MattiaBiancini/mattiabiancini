64302e7b46b92a817498caac5cad96257f7f8de1
Biancini Mattia <m.biancini1@campus.unimib.it>
2025-06-08 12:15:27 +0200

    First Commit Contacts and Swimming Pages

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,53 @@
+import {
+  Avatar,
+  Background,
+  Button,
+  Column,
+  Flex,
+  Heading,
+  Icon,
+  IconButton,
+  Row,
+  SmartImage,
+  Tag,
+  Text,
+} from "@/once-ui/components";
+import { baseURL } from "@/app/resources";
+import { about, person, projects } from "@/app/resources/content";
+import { Meta, Schema } from "@/once-ui/modules";
+import OverlappingAvatars from "@/components/projects/OverlappingAvatars";
+
+export async function generateMetadata() {
+  return Meta.generate({
+    title: projects.title,
+    description: projects.description,
+    baseURL: baseURL,
+    image: `${baseURL}/og?title=${encodeURIComponent(projects.title)}`,
+    path: projects.path,
+  });
+}
+
+export default function Contacts() {
+	return (
+		<Column maxWidth="m">
+			<Schema
+				as="webPage"
+				baseURL={baseURL}
+				path={projects.path}
+				title={projects.title}
+				description={projects.description}
+				image={`${baseURL}/og?title=${encodeURIComponent(projects.title)}`}
+				author={{
+				name: person.name,
+				url: `${baseURL}${about.path}`,
+				image: `${baseURL}${person.avatar}`,
+				}}
+			/>
+			<Column style={{ gap: "2em" }}>
+				<Text variant="heading-default-l" onBackground="neutral-weak">
+					WIP
+				</Text>
+			</Column>
+		</Column>
+	);
+}
