f64598a69040256c5d687bec533a7d04c88f0181
Biancini Mattia <m.biancini1@campus.unimib.it>
2025-06-08 12:22:48 +0200

    Visual Update Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
               Some of my interests
             </Heading>
           </RevealFx>
-          <RevealFx paddingTop="8" delay={0.2} horizontal="start" paddingLeft="12">
+          <RevealFx paddingTop="8" delay={0.2} horizontal="start" paddingLeft="16" paddingRight="16">
             <Text onBackground="neutral-weak" variant="label-default-m" align="justify">
               If I am not working on my projects, you can find me either swimming in the pool,
               playing team-based games, or learning.
